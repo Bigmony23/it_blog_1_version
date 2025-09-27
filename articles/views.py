25520620fa4aa2a26512a5046dff4bc051136d68
New <dierbekergasev6@gmail.com>
2025-09-27 14:25:22 +0500

    Remove pyc files and add .gitignore adding authorization and improving design

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from articles.models import Article
 
 
@@ -13,19 +13,33 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_view.html'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-class ArticleUpdateView(UpdateView):
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.author or self.request.user.is_superuser
+
+class ArticleUpdateView( LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Article
     fields = ['title','summary','body','photo']
     success_url = reverse_lazy('article_list')
     template_name = 'article_edit.html'
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author==self.request.user or self.request.user.is_superuser
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,UserPassesTestMixin,CreateView):
     model = Article
-    fields = ['title','summary','body','photo','author']
+    fields = ['title','summary','body','photo']
     success_url = reverse_lazy('article_list')
     template_name = 'article_new.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user.is_superuser
